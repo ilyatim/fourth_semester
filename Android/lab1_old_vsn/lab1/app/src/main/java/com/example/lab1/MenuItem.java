46a2366f3e50d4d278a6b3c14ed152878cf18f42
ilyatim <31735653+ilyatim@users.noreply.github.com>
2019-03-23 20:24:10 +0300

    Update

--- Android/lab1_old_vsn/lab1/app/src/main/java/com/example/lab1/MenuItem.java
+++ Android/lab1_old_vsn/lab1/app/src/main/java/com/example/lab1/MenuItem.java
@@ -16,19 +16,12 @@ public class MenuItem
     {
         this.number = convert(number);
     }
-    public MenuItem()
-    {
-        this.number = "Пусто";
-    }
 
-    public String getNumber()
+    public String getNumber() //получение значения из числа
     {
         return number;
     }
-    public void setNumber(int number)
-    {
-        this.number = convert(number);
-    }
+
     public String convert(int number)
     {
         //проверяю сколько чисел в числе
@@ -36,7 +29,7 @@ public class MenuItem
         //возвращаю конвертированое число
         int value = number;
         int count = 0;
-        while(value > 0)
+        while(value > 0) //проверка на количество чисел
         {
             value /= 10;
             count++;
@@ -44,21 +37,21 @@ public class MenuItem
         switch(count)
         {
             case 1:
-                return decades(number);
+                return decades(number); //вызов функции для 1-го символа
             case 2:
-                return hundreds(number);
+                return hundreds(number); //вызов функции для 2-х символом
             case 3:
-                return chiliad(number);
+                return chiliad(number); //вызов функции для 3-х символом
             case 4:
-                return thousand(number);
+                return thousand(number); //вызов функции для 4-х символом
             case 5:
-                return tensOfThousands(number);
+                return tensOfThousands(number); //вызов функции для 5-ти символом
             case 6:
-                return hundredsOfThousands(number);
+                return hundredsOfThousands(number); //вызов функции для 6-ти символом
             case 7:
                 return million;
             default:
-                return zero;
+                return zero; //если количество символом больше 7, возвращается "ноль"
         }
     }
     private String decades(int number) // функция возвращает значение числа с 1 цифрой
@@ -72,18 +65,15 @@ public class MenuItem
             String num = hundreds_2[(number % 10) - 1];
             return num;
         }
+        else if(number % 10 == 0)
+        {
+            String num = hundreds[(number / 10) - 1];
+            return num;
+        }
         else
         {
-            if(number % 10 == 0)
-            {
-                String num = hundreds[(number / 10) - 1];
-                return num;
-            }
-            else
-            {
-                String num = hundreds[(number / 10) - 1] + " " +  decades(number % 10);
-                return num;
-            }
+            String num = hundreds[(number / 10) - 1] + " " +  decades(number % 10);
+            return num;
         }
     }
     private String chiliad(int number) // функция возвращает значение числа с 3 цифрами
@@ -95,13 +85,11 @@ public class MenuItem
             String num = chiliad[(number / 100) - 1] + " " + decades(number % 100);
             return num;
         }
-        else if(number % 100 > 10 && number % 100 < 100)
+        else
         {
             String num = chiliad[(number / 100) - 1] + " " + hundreds(number % 100);
             return num;
         }
-        else
-            return zero;
     }
     private String thousand(int number) // функция возвращает значение числа с 4 цифрами
     {
@@ -112,7 +100,7 @@ public class MenuItem
             String num = thousand[(number / 1000) - 1] + " " + decades(number % 1000);
             return num;
         }
-        else if(number % 1000 > 10 && number % 1000 < 100)
+        else if(number % 1000 > 9 && number % 1000 < 100)
         {
             String num = thousand[(number / 1000) - 1] + " " + hundreds(number % 1000);
             return num;
@@ -123,30 +111,78 @@ public class MenuItem
             return num;
         }
     }
-    private String tensOfThousands(int number) // функция возвращает значение числа с 5 цифрами
+    private String thousand_2(int number) // функция возвращает значение числа с 4 цифрами
     {
-        if(number % 10000 == 0)
-            return hundreds[number / 10000 - 1] + thousands;
-        else if(number % 10000 > 999)
+        if(number % 1000 == 0)
+            return thousands;
+        else if(number % 1000 < 10)
         {
-            String num = hundreds[number / 10000 - 1] + thousands + " " + thousand(number % 10000);
+            String num = thousands + " " + decades(number % 1000);
             return num;
         }
-        else if(number % 10000 > 99 && number % 10000 < 1000)
+        else if(number % 1000 > 9 && number % 1000 < 100)
         {
-            String num = hundreds[number / 10000 - 1] + thousands + " " +  chiliad(number % 10000);
-            return num;
-        }
-        else if(number % 10000 > 9 && number % 10000 < 100)
-        {
-            String num = hundreds[number / 10000 - 1] + thousands + " " +  hundreds(number % 10000);
+            String num = thousands + " " + hundreds(number % 1000);
             return num;
         }
         else
         {
-            String num = hundreds[number / 10000 - 1] + thousands + " " +  decades(number % 10000);
+            String num = thousands + " " + chiliad(number % 1000);
             return num;
         }
+    }
+    private String tensOfThousands(int number) // функция возвращает значение числа с 5 цифрами
+    {
+        if((number / 1000) > 10 && (number / 1000) < 20)
+        {
+            if(number % 10000 < 10)
+            {
+                String num = hundreds_2[(number / 1000) % 10 - 1] + " " +  decades(number % 10000);
+                return num;
+            }
+            else if(number % 10000 > 9 && number % 10000 < 100)
+            {
+                String num = hundreds_2[(number / 1000) % 10 - 1] + " " +  hundreds(number % 10000);
+                return num;
+            }
+            else if(number % 10000 > 99 && number % 10000 < 1000)
+            {
+                String num = hundreds_2[(number / 1000) % 10 - 1] + " " +  chiliad(number % 10000);
+                return num;
+            }
+            else
+            {
+                String num = hundreds_2[(number / 1000) % 10 - 1] + " " + thousand_2(number % 10000);
+                return num;
+            }
+        }
+        else
+        {
+            if(number % 10000 == 0)
+                return hundreds[number / 10000 - 1] + thousands;
+            else if(number % 10000 < 10)
+            {
+                String num = hundreds[number / 10000 - 1] + thousands + " " +  decades(number % 10000);
+                return num;
+            }
+            else if(number % 10000 > 9 && number % 10000 < 100)
+            {
+                String num = hundreds[number / 10000 - 1] + thousands + " " +  hundreds(number % 10000);
+                return num;
+            }
+            else if(number % 10000 > 99 && number % 10000 < 1000)
+            {
+                String num = hundreds[number / 10000 - 1] + thousands + " " +  chiliad(number % 10000);
+                return num;
+            }
+            else
+            {
+                String num = hundreds[number / 10000 - 1] + thousands + " " + thousand(number % 10000);
+                return num;
+            }
+        }
+
+
     }
     private String hundredsOfThousands(int number) // функция возвращает значение числа с 6 цифрами
     {
@@ -154,28 +190,29 @@ public class MenuItem
             return chiliad[number / 100000 - 1] + thousands;
         else if(number % 100000 > 9999)
         {
-            String num = chiliad[number / 100000 - 1] + tensOfThousands(number % 100000);
+            String num = chiliad[number / 100000 - 1] + " " + tensOfThousands(number % 100000);
             return num;
         }
         else if(number % 100000 > 999 && number % 10000 < 10000)
         {
-            String num = chiliad[number / 100000 - 1] + thousand(number % 100000);
+            String num = chiliad[number / 100000 - 1] + " " + thousands + " " + thousand(number % 100000);
             return num;
         }
         else if(number % 100000 > 99 && number % 10000 < 1000)
         {
-            String num = chiliad[number / 100000 - 1] + chiliad(number % 100000);
+            String num = chiliad[number / 100000 - 1] + " " + thousands + " " + chiliad(number % 100000);
             return num;
         }
         else if(number % 100000 > 9 && number % 10000 < 100)
         {
-            String num = chiliad[number / 100000 - 1] + hundreds(number % 100000);
+            String num = chiliad[number / 100000 - 1] + " " + thousands + " " + hundreds(number % 100000);
             return num;
         }
         else
         {
-            String num = chiliad[number / 100000 - 1] + decades(number % 100000);
+            String num = chiliad[number / 100000 - 1] + " " + thousands + " " + decades(number % 100000);
             return num;
         }
     }
+
 }
